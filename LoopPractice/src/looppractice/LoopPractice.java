9144ad88346d32d6b3f8bf54769f407fde97c0c8
mara Rosa <edimara.rosa@hotmail.com>
2021-03-01 21:50:34 -0300

    day 05

--- LoopPractice/src/looppractice/LoopPractice.java
+++ LoopPractice/src/looppractice/LoopPractice.java
@@ -1,4 +1,3 @@
-
 package looppractice;
 
 /**
@@ -14,7 +13,7 @@ public class LoopPractice {
             x++;
         }
     }
-    
+
     public static void practiceDoWhileLoop() {
         int x = 0;
         do {
@@ -22,28 +21,37 @@ public class LoopPractice {
             x++;
         } while (x < 10);
     }
-    
+
     public static void practiceForLoop() {
         for (int x = 0; x < 20; x++) {
-             System.out.println("The value of x is " + x);
-             if (x == 10) {
-                 break;
-             }
+            System.out.println("The value of x is " + x);
+            if (x == 10) {
+                break;
+            }
         }
         System.out.println("This is the end");
     }
-    
-        
+
     public static void practiceForLoopDecrement() {
         for (int x = 10; x > 0; x--) {
-             System.out.println("The value of x is " + x);
+            System.out.println("The value of x is " + x);
         }
         System.out.println("This is the end");
     }
-    
+
+    public static void practiceNestedForLoop() {
+        for (int x = 0; x < 10; x++) {
+               System.out.println("Started For loop x");
+            for (int y = 0; y < 10; y++) {
+                System.out.println("(" + x + "," + y + ")");
+            }
+            System.out.println("For loop y ended");
+        }
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
-        practiceForLoopDecrement();
+        practiceNestedForLoop();
     }
-    
+
 }
