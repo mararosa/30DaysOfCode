a9c7af7c1640b3cdf3a50233f71dd6f4ab9e49a1
mara Rosa <edimara.rosa@hotmail.com>
2021-03-07 17:52:12 -0300

    Day 07

--- ArrayDemo/src/arraydemo/ArrayDemo.java
+++ ArrayDemo/src/arraydemo/ArrayDemo.java
@@ -1,4 +1,3 @@
-
 package arraydemo;
 
 import java.util.Arrays;
@@ -8,16 +7,30 @@ public class ArrayDemo {
     public static void main(String[] args) {
 
         // initializing int array
-        int[] array = new int[] {10,22,36};
-        
+        int[] array = new int[]{10, 22, 36};
+
         // let us print all the elements available in list
         System.out.println("The array is:");
         for (int number : array) {
             System.out.println("Number = " + number);
         }
-        
+
         System.out.println("The string representation of array is:");
         System.out.println(Arrays.toString(array));
+
+        // the number of elements we want to hold
+        final int _arraySize = 4;
+
+// our array declaration
+        String[] stringArray = new String[_arraySize];
+
+        for (int i = 0; i < _arraySize; i++) {
+            // assign value to index i
+            stringArray[i] = "This is stored in index " + i;
+
+            // print value saved in index i
+            System.out.println(stringArray[i]);
+        }
     }
-    
+
 }
