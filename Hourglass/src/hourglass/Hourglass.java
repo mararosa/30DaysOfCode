1e23e2f75d7c9c49a407b64c406216bd2741482f
mara Rosa <edimara.rosa@hotmail.com>
2021-03-17 21:52:29 -0300

    understanding 2D array

--- Hourglass/src/hourglass/Hourglass.java
+++ Hourglass/src/hourglass/Hourglass.java
@@ -13,13 +13,13 @@ public class Hourglass {
 
     // Java program to find maximum 
 // sum of hour glass in matrix 
-    static int R = 5;
-    static int C = 5;
+    static int ROW = 5;
+    static int COLUMN = 5;
 
 // Returns maximum sum of 
 // hour glass in ar[][]
-    static int findMaxSum(int[][] mat) {
-        if (R < 3 || C < 3) {
+    static int findMaxSum(int[][] arrayTwoDimensional) {
+        if (ROW < 3 || COLUMN < 3) {
             return -1;
         }
 
@@ -27,14 +27,14 @@ public class Hourglass {
         // times considering different
         // top left cells of hour glasses.
         int max_sum = Integer.MIN_VALUE;
-        for (int i = 0; i < R - 2; i++) {
-            for (int j = 0; j < C - 2; j++) {
+        for (int i = 0; i < ROW - 2; i++) {
+            for (int j = 0; j < COLUMN - 2; j++) {
                 // Considering mat[i][j] as top 
                 // left cell of hour glass.
-                int sum = (mat[i][j] + mat[i][j + 1]
-                        + mat[i][j + 2]) + (mat[i + 1][j + 1])
-                        + (mat[i + 2][j] + mat[i + 2][j + 1]
-                        + mat[i + 2][j + 2]);
+                int sum = (arrayTwoDimensional[i][j] + arrayTwoDimensional[i][j + 1]
+                        + arrayTwoDimensional[i][j + 2]) + (arrayTwoDimensional[i + 1][j + 1])
+                        + (arrayTwoDimensional[i + 2][j] + arrayTwoDimensional[i + 2][j + 1]
+                        + arrayTwoDimensional[i + 2][j + 2]);
 
                 // If previous sum is less then 
                 // current sum then update
