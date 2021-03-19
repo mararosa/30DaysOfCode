777b40162cd6be4db019748e85365869108ac951
mara Rosa <edimara.rosa@hotmail.com>
2021-03-19 00:10:10 -0300

    day 12

---
+++ Animal/src/animal/Animal.java
@@ -0,0 +1,37 @@
+
+package animal;
+
+public class Animal {
+    
+    private int age;
+
+    public Animal(int age) {
+        this.age = age;
+        System.out.println("An animal has been created!");
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void eat() {
+        System.out.println("An anmal is eating");
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Animal a = new Animal(5);
+        Dog d = new Dog();
+        Cat c = new Cat();
+        d.ruff();
+        System.out.println(d.getAge());
+        c.meow();
+        System.out.println(c.getAge());
+        a.eat();
+        d.eat();
+        c.eat();
+        d.run();
+        c.prance();
+    }
+    
+}
