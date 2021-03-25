2b7810e9e27a4e6f350e389b7ce29a113dab5e18
mara Rosa <edimara.rosa@hotmail.com>
2021-03-24 23:57:18 -0300

    day 14

---
+++ Difference/src/difference/Difference.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package difference;
+
+/**
+ *
+ * @author mararosa
+ */
+public class Difference {
+
+    private int[] elements;
+    public int maximumDifference;
+    public int minimumValue = Integer.MAX_VALUE ;
+    public int maxValue = Integer.MIN_VALUE ;
+
+    // Add your code here
+    Difference(int[] arr) {
+        this.elements = arr;
+    }
+
+    void computeDifference() {
+        for (int i = 0; i < elements.length; i++) {
+            maxValue = Math.max(maxValue, elements[i]);
+            minimumValue = Math.min(minimumValue, elements[i]);
+        }
+        maximumDifference = (maxValue - minimumValue);
+        System.out.println(maximumDifference);
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arr = {5, 2, 9, 1, 3};
+        Difference difference = new Difference(arr);
+        difference.computeDifference();
+    }
+
+}
