2d2fa78f04e218eaefc65d4772df46b890579584
mara Rosa <edimara.rosa@hotmail.com>
2021-03-29 17:51:42 -0300

    day 17

--- HotChocolate/src/hotchocolate/HotChocolate.java
+++ HotChocolate/src/hotchocolate/HotChocolate.java
@@ -7,10 +7,12 @@ public class HotChocolate {
     public static final double tooHot = 185;
     public static final double tooCold = 160;
 
-    public static void drinkotChocolate(double cocoaTemperature) throws TooHotException, TooColdException {
+    public static void drinkHotChocolate(double cocoaTemperature) throws TooHotException, TooColdException {
         if (cocoaTemperature >= tooHot) {
+            System.out.println("THAT'S TOO HOT!");
             throw new TooHotException();
         } else if (cocoaTemperature <= tooCold) {
+              System.out.println("THAT'S SO COLD! It's like the artic");
             throw new TooColdException();
         }
     }
@@ -18,18 +20,18 @@ public class HotChocolate {
     public static void main(String[] args) throws TooColdException,
             TooHotException, InterruptedException {
         // TODO code application logic here
-        double currentCocoaTemp = 170;
+        double currentCocoaTemp = 200;
         boolean wrongTemp = true;
         while (wrongTemp) {
             try {
-                drinkotChocolate(currentCocoaTemp);
+                drinkHotChocolate(currentCocoaTemp);
                 System.out.println("That cocoa was good!");
                 wrongTemp = false;
             } catch (TooHotException e) {
-                System.out.println("THAT'S TOO HOT!");
+                
                 currentCocoaTemp -= 5;
             } catch (TooColdException e) {
-                System.out.println("THAT'S SO COLD! It's like the artic");
+              
                 currentCocoaTemp += 5;
             }
             TimeUnit.SECONDS.sleep(2);
