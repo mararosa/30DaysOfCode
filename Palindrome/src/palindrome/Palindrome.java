048fe260c310323406f5b5af0b7cd74ce31ad303
mara Rosa <edimara.rosa@hotmail.com>
2021-03-31 00:19:38 -0300

    Palindrome

---
+++ Palindrome/src/palindrome/Palindrome.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palindrome;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mararosa
+ */
+public class Palindrome {
+
+    LinkedList queue;
+    LinkedList stack;
+
+    public Palindrome() {
+        queue = new LinkedList();
+        stack = new LinkedList();
+    }
+
+    public LinkedList getQueue() {
+        return queue;
+    }
+
+    public LinkedList getStack() {
+        return stack;
+    }
+
+    public void setQueue(LinkedList newQueue) {
+        queue = newQueue;
+    }
+
+    public void setStack(LinkedList newStack) {
+        stack = newStack;
+    }
+
+    public void pushCharacter(char ch) {
+        getStack().addLast(ch);
+    }
+
+    public void enqueueCharacter(char ch) {
+        getQueue().addLast(ch);
+    }
+
+    public char popCharacter() {
+        return (char) getStack().removeLast();
+    }
+
+    public char dequeueCharacter() {
+        return (char) getQueue().removeFirst();
+    }
+
+    public int size() {
+        return getStack().size();
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Let's check if the word is a palindrome. Type something: ");
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] StringToCharArray = input.toCharArray();
+
+        Palindrome p = new Palindrome();
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : StringToCharArray) {
+            p.enqueueCharacter(c);
+            p.pushCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < StringToCharArray.length/2; i++) {
+            if (p.dequeueCharacter() != p.popCharacter()) {
+                isPalindrome =  false;
+                System.out.println(p.getStack());
+                System.out.println(p.getQueue());
+                break;
+            }
+        }
+        
+        System.out.println("The word, " + input + ", is " + ((!isPalindrome) ? "not a "
+                + "palindrome." : "a palindrome."));  
+    }
+
+}
